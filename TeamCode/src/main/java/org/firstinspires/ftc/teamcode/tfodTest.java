2cdc19cc836f99e332bf91c3fc78b966d1f9d366
Nathaniel <wallywilly63@gmail.com>
2020-10-18 16:41:53 -0500

    More tfod work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tfodTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tfodTest.java
@@ -10,7 +10,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
 
 //@Disabled
 @Autonomous(name="Tensor Flow Object Detection",group="Competition")
@@ -71,8 +74,48 @@ public class tfodTest extends LinearOpMode {
 
             waitForStart();
 
+            if (tfod != null) {
 
+                ElapsedTime timeOut = new ElapsedTime();
+                timeOut.reset();
 
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                while ((tfod.getUpdatedRecognitions() != null) && (timeOut.seconds() < 5)) {
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                            if(recognition.getLabel() == "Single") {
+                                moveTime(0, 1, 0, 3);
+                                telemetry.addLine("MOVING TO TARGET B");
+                            } else if (recognition.getLabel() == "Quad") {
+                                moveTime(0, 3, 0, 3000);
+                                moveTime(-1, 1, 0, 1410);
+                                telemetry.addLine("MOVING TO TARGET C");
+
+                            } else {
+                                moveTime(0, 1, 0, 2000);
+                                moveTime(-1, 1, 0, 1410);
+                                telemetry.addLine("MOVING TO TARGET A");
+                            }
+                            telemetry.update();
+                        }
+                        telemetry.update();
+                    }
+                }
+
+                tfod.getUpdatedRecognitions();
+
+            }else {telemetry.addLine("WARNING! TFOD INIT FAILURE"); telemetry.update();}
 
         }
 
