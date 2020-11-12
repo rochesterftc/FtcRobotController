334d8cbc4a6da617ff1435662d061d358c83aedc
Squirlsand <trumanschudel@gmail.com>
2020-11-11 18:20:08 -0600

    Updated the Holonomic chassis to include launch functions, and conveyor functions. Refined teleOp arm, and Intake controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
@@ -2,10 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+import org.firstinspires.ftc.robotcore.internal.android.dex.Code;
+import org.firstinspires.ftc.teamcode.hardwaremap.HardwareHolonomicChassis;
+
+import static java.lang.Thread.sleep;
+
 /**
  * Created by Rochesterftc10303 on 10/4/2018.
  */
@@ -13,16 +22,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class CompetitionTeleOp extends OpMode {
 
-    DcMotor fl;
-    DcMotor fr;
-    DcMotor bl;
-    DcMotor br;
-    DcMotor lift;
-    DcMotor launch;
-    DcMotor intakemotor;
-    CRServo intakeservo;
-    DcMotor arm;
-    Servo claw;
+    HardwareHolonomicChassis robot = new HardwareHolonomicChassis();
+
     boolean clawButtonPushed;
     boolean clawOn;
     boolean IsButtonPushed;
@@ -31,24 +32,17 @@ public class CompetitionTeleOp extends OpMode {
     boolean ArmOn;
     boolean intakeButtonPushed;
     boolean IntakeOn;
+    double  armDirection;
+    double  intakeMotorDirection;
 
     // Declare OpMode members.
    // private boolean helloThereFound;      // Sound file present flag
 
     public void init() {
+        robot.init(hardwareMap);
         // Test Branch
         // Determine Resource IDs for sounds built into the RC application.
 
-        fl = hardwareMap.dcMotor.get("front left");
-        fr = hardwareMap.dcMotor.get("front right");
-        bl = hardwareMap.dcMotor.get("back left");
-        br = hardwareMap.dcMotor.get("back right");
-        lift = hardwareMap.dcMotor.get("lift");
-        launch = hardwareMap.dcMotor.get("launch");
-        intakemotor = hardwareMap.dcMotor.get("IM");
-        intakeservo = hardwareMap.crservo.get("IS");
-        arm = hardwareMap.dcMotor.get("arm");
-        claw = hardwareMap.servo.get("claw");
 
     }
 
@@ -57,49 +51,63 @@ public class CompetitionTeleOp extends OpMode {
         //5:1 140 counts per rotation
 
         if (gamepad2.b && !armButtonPushed) {
-            arm.setPower((ArmOn ? 1 : 0));
+            // ((ArmOn ? 21 : -21));
+             if (ArmOn) {double armDirection = 55;}
+             else {double armDirection = -55;}
             ArmOn = !ArmOn;
             armButtonPushed = true;
         } else if (!gamepad2.y && armButtonPushed) armButtonPushed = false;
 
+        if (gamepad2.b) {
+            robot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.arm.setTargetPosition((int)armDirection);
+        }
+
         if (gamepad2.b && !intakeButtonPushed) {
-            arm.setPower((IntakeOn ? 1 : 0));
+          //  robot.intakemotor.setPower((IntakeOn ? 1 : -1));
+            if (ArmOn) {double intakeMotorDirection = -30;}
+            else {double intakeMotorDirection = 30;}
             IntakeOn = !IntakeOn;
             armButtonPushed = true;
         } else if (!gamepad2.y && intakeButtonPushed) intakeButtonPushed = false;
-
+        if (gamepad2.b) {
+            robot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.arm.setTargetPosition((int)intakeMotorDirection);
+        }
 
         float x = gamepad1.left_stick_x;
         float z = -gamepad1.right_stick_x;
         float y = -gamepad1.left_stick_y;
 
 
-        fl.setPower(y - x - z);
-        fr.setPower(-y - x - z);
-        bl.setPower(y + x - z);
-        br.setPower(-y + x - z);
+        robot.fl.setPower(y - x - z);
+        robot.fr.setPower(-y - x - z);
+        robot.bl.setPower(y + x - z);
+        robot.br.setPower(-y + x - z);
 
 
         if (gamepad2.y && !clawButtonPushed) {
-            claw.setPosition((clawOn ? 0.7 : 0.3));
+            robot.claw.setPosition((clawOn ? 0.7 : 0.3));
             clawOn = !clawOn;
             clawButtonPushed = true;
         } else if (!gamepad2.y && clawButtonPushed) clawButtonPushed = false;
 
         if (gamepad2.x && !IsButtonPushed) {
-            intakeservo.setPower((IsOn ? 1:0));
+            robot.intakeservo.setPower((IsOn ? 1:0));
             IsOn = !IsOn;
             IsButtonPushed = true;
         } else if (!gamepad2.x && IsButtonPushed) IsButtonPushed = false;
 
-        lift.setPower(gamepad2.left_trigger);
-        launch.setPower(gamepad2.right_trigger);
+        robot.Conveyor.setPower(gamepad2.left_trigger);
+        robot.shooter.setPower(gamepad2.right_trigger);
 
         if (gamepad2.left_bumper) {
-            launch.setPower(.9);
+            robot.shooter.setPower(.9);
         }
         if (gamepad2.right_bumper){
-            launch.setPower(.1);
+            robot.shooter.setPower(.1);
         }
     }
 }
