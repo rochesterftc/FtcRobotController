0932ed6747d1a3c988387c8d8c716c1135fe5e90
saddlepiggy <55365185+saddlepiggy@users.noreply.github.com>
2020-12-06 16:28:26 -0600

    FINALLY FIXED ENCODERS YAYYYYYYYYYYYYY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/CompetitionAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/CompetitionAutonomous.java
@@ -10,13 +10,13 @@ import org.firstinspires.ftc.teamcode.hardwaremap.HardwareHolonomicChassis;
  * Created by George on 11/9/2020.
  */
 
-@Autonomous(name = "Compeition Autonomous", group = "Competition")
+@Autonomous(name = "Competiton Autonomous", group = "Competition")
 
 public class CompetitionAutonomous extends LinearOpMode {
 
     HardwareHolonomicChassis robot = new HardwareHolonomicChassis();
 
-    int var;
+    int var = 1;
 
     @Override
     public void runOpMode() {
@@ -29,14 +29,15 @@ public class CompetitionAutonomous extends LinearOpMode {
 
         //move to the white shooter line, then the corner of the white line and 1st blue box
         if (var == 1 || var == 3) {
+            //Backup
             do {
                 robot.fr.setPower(-.5);
                 robot.fl.setPower(.5);
                 robot.br.setPower(-.5);
                 robot.bl.setPower(.5);
             }
+            //While the color sensor does not detect white
             while (robot.sensorColor.red() < 200 || robot.sensorColor.green() < 200 || robot.sensorColor.blue() < 200);
-
             robot.fr.setPower(0);
             robot.fl.setPower(0);
             robot.br.setPower(0);
@@ -44,28 +45,30 @@ public class CompetitionAutonomous extends LinearOpMode {
 
             sleep(250);
 
+            //Drive to the right
             do {
                 robot.fr.setPower(-.5);
                 robot.fl.setPower(-.5);
                 robot.br.setPower(.5);
                 robot.bl.setPower(.5);
             }
+            //While the color sensor does not detect blue
             while (robot.sensorColor.blue() < robot.sensorColor.red() || robot.sensorColor.blue() < robot.sensorColor.green());
-
             robot.fr.setPower(0);
             robot.fl.setPower(0);
             robot.br.setPower(0);
             robot.bl.setPower(0);
         }
         if (var == 2) {
+            //Drive backward
             do {
                 robot.fr.setPower(-.5);
                 robot.fl.setPower(.5);
                 robot.br.setPower(-.5);
                 robot.bl.setPower(.5);
             }
+            //While the color sensor does not detect white
             while (robot.sensorColor.red() < 200 || robot.sensorColor.green() < 200 || robot.sensorColor.blue() < 200);
-
             robot.fr.setPower(0);
             robot.fl.setPower(0);
             robot.br.setPower(0);
@@ -73,14 +76,15 @@ public class CompetitionAutonomous extends LinearOpMode {
 
             sleep(250);
 
+            //Drive left
             do {
                 robot.fr.setPower(.5);
                 robot.fl.setPower(.5);
                 robot.br.setPower(-.5);
                 robot.bl.setPower(-.5);
             }
+            //While the color sensor does not detect blue
             while (robot.sensorColor.blue() < robot.sensorColor.red() || robot.sensorColor.blue() < robot.sensorColor.green());
-
             robot.fr.setPower(0);
             robot.fl.setPower(0);
             robot.br.setPower(0);
